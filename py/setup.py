b60c07583628f7cb3a9b370f696544fa787d0568
Joey McCollum <jpmccollum05834@yahoo.com>
2019-06-24 13:29:38 -0400

    Update setup.py

--- py/setup.py
+++ py/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='jude-nmf',
-	version='1.0.0',
+    version='1.0.0',
     packages=find_packages(),
     description='Non-negative matrix factorization applied to Wasserman\'s collation of the epistle of Jude.',
     author='Joey McCollum',
     license='MIT',
     author_email='jjmccollum@vt.edu',
     url='https://github.com/jjmccollum/jude-nmf',
-	python_requires='>=3.5',
+    python_requires='>=3.5',
     install_requires=[
         'pandas',
         'sklearn',
